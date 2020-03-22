fffecdfa69b2cc33758881523a8c2f2b6c51ca04
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-03-22 17:11:47 +0800

    Create to_do_list.py

---
+++ to_do_list.py
@@ -0,0 +1,39 @@
+----------------------------------------------------
+Level 1: Backtracking
+----------------------------------------------------
+Classic = DONE
+X = DONE
+King = DONE
+Knight = DONE
+Queen = DONE
+
+Sandwich = Pending
+Little Killer = Pending
+
+Jigsaw = Keep in view
+Thermo = Keep in view
+Killer = Keep in view
+Skyscraper = Keep in view
+Frame = Keep in view
+Windoku = Keep in view
+Scattered = Keep in view
+Samurai = Keep in view
+
+----------------------------------------------------
+Level 2: Constraint Satisfaction Problem
+----------------------------------------------------
+
+
+----------------------------------------------------
+Level 3: React Native
+----------------------------------------------------
+
+
+
+----------------------------------------------------
+Level 4: App Store
+----------------------------------------------------
+
+----------------------------------------------------
+Level 5: Be a Gazillionaire
+----------------------------------------------------
