d49364c2bfb1ec7cc388d5c8cb504fae051f2af1
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-03-22 15:56:47 +0800

    Update king_sudoku.py

--- king_sudoku.py
+++ king_sudoku.py
@@ -42,7 +42,8 @@ def possible_movement(x,y,n):
 
     # Custom sudoku rule: King Sudoku 
     if king_sudoku:
-        knight_move = [ [x + 1, y + 1], [x + 1 , y - 1] , [x - 1, y + 1], [x - 1, y - 1], [x + 1, y + 1], [x + 1 , y - 1] , [x - 1, y + 1], [x - 1, y - 1] ]
+        knight_move = [ [x + 1, y + 1], [x + 1 , y - 1] , [x - 1, y + 1], [x - 1, y - 1], 
+                       [x + 1, y + 1], [x + 1 , y - 1] , [x - 1, y + 1], [x - 1, y - 1] ]
         for element in knight_move:
             if element[0] in [0,1,2,3,4,5,6,7,8]:
                     if element[1] in [0,1,2,3,4,5,6,7,8]:
